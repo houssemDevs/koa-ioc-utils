d4afd96905088028e3bd7cf745f0513e8ce6ea00
GHIAT houssem <ghiat.houssem@gmail.com>
2019-04-28 11:42:40 +0200

    add comments

--- src/base_middleware.ts
+++ src/base_middleware.ts
@@ -1,5 +1,10 @@
 import { ParameterizedContext } from 'koa';
 
+/**
+ * A Middleware abstract class intended to be used
+ * for middlewares that need DI capabilities when get
+ * constructed.
+ */
 export abstract class BaseMiddleware<TState = any, TCustom = {}> {
   public abstract async handle(ctx: ParameterizedContext<TState, TCustom>, next: () => Promise<any>): Promise<any>;
 }
